823112a6065c95a4e353ca8a9330849b86809f40
Vanessa Vasquez <vlvasquez23sa@gmail.com>
2021-04-22 23:13:11 -0500

    App Update

--- static/js/app.js
+++ static/js/app.js
@@ -1,97 +1,108 @@
-function buildMetadata(sample.data) {
-
-    // @TODO: Complete the following function that builds the metadata panel
+function optionChanged() {
   
-    // Use `d3.json` to fetch the metadata for a sample
-    var metadataURL = `/metadata/${sample}`;
-      // Use d3 to select the panel with id of `#sample-metadata`
-      d3.json(metadataURL).then(function(sample){
-        var sampleData = d3.select(`#sample-metadata`);
-      // Use `.html("") to clear any existing metadata
-        sampleData.html("");
-      // Use `Object.entries` to add each key and value pair to the panel
-        Object.entries(sample).forEach(function([key,value]){
-          var row = sampleData.append("p");
-          row.text(`${key}:${value}`)
-        })
-      });
-  }
-  
-  function buildCharts(sample) {
-    //  Use `d3.json` to fetch the sample data for the plots
-    var plotData = `/samples/${sample}`;
-    //  Build a Bubble Chart using the sample data
-    d3.json(plotData).then(function(data){
-      var x_axis = data.otu_ids;
-      var y_axis = data.sample_values;
-      var size = data.sample_values;
-      var color = data.otu_ids;
-      var texts = data.otu_labels;
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("selDataset").node();
     
-      var bubble = {
-        x: x_axis,
-        y: y_axis,
-        text: texts,
-        mode: `markers`,
-        marker: {
-          size: size,
-          color: color
+    // Assign the dropdown menu option to a variable
+    var selectedOption = dropdownMenu.value;
+
+    console.log(selectedOption);
+
+    optionChanged();
+};
+
+
+function getData() {
+
+    // Read the JSON file
+    d3.json('samples.json').then(function(data) {
+
+        // Create variable for the dropdown (select tag)
+        var select = d3.select('select');
+
+        // Append option tags for each ID in the names array
+        Object.entries(data.names).forEach(function([key, value]) {
+            var option = select.append('option')
+            option.text(value)});
+
+        // Create variable for subject of interest
+        var selectedOption = document.getElementById('selDataset');
+        var selectedOption = selectedOption.options[selectedOption.selectedIndex].value;
+        console.log(selectedOption)
+
+        // Create a function that returns metadata based on id number
+        function filterData(person) {
+            return person.id == selectedOption;
+        };
+
+        // Filter metadata by subject of interest
+        var filteredData = data.metadata.filter(filterData);
+        console.log(filteredData[0]);
+
+        // Create a variable for the Demographic Info panel
+        var panel = d3.select('panel-body');
+
+        // Initialize panel
+        panel.selectAll('*').remove();
+
+        // Append information on subject based on dropdown value
+        Object.entries(filteredData[0]).forEach(function([key, value]) {
+            var row = panel.append('tr')
+            row.text(`${key}: ${value}`);
+        })
+        
+        // Filter sample data by subject of interest
+        var filteredSample = data.samples.filter(filterData);
+        var filteredSample = filteredSample[0];
+        console.log(filteredSample);
+
+        // Pull all data from samples for bubble chart plotting
+        sampleIDs = filteredSample.otu_ids;
+        sampleValues = filteredSample.sample_values;
+        sampleLabels = filteredSample.otu_labels;
+
+        // Slice the first 10 objects for bar chart plotting
+        slicedIDs = filteredSample.otu_ids.slice(0, 10);
+        slicedValues = filteredSample.sample_values.slice(0, 10);
+        slicedLabels = filteredSample.otu_labels.slice(0, 10);
+        console.log(slicedIDs, slicedValues, slicedLabels)
+
+        // Trace1 for the horizontal bar chart
+        var trace1 = {
+            x: slicedValues,
+            y: slicedIDs.toString(),
+            text: slicedLabels,
+            type: 'bar',
+            orientation: 'h'
+        };
+
+        var hbarChart = [trace1];
+
+        var layout1 = {
+            title: 'Top 10 Samples'
+        };
+
+        Plotly.newPlot('bar', hbarChart, layout1)
+
+        // Trace2 for the bubble chart
+        var trace2 = {
+            x: sampleIDs,
+            y: sampleValues,
+            text: sampleLabels,
+            mode: 'markers',
+            marker: {
+                color: sampleIDs,
+                size: sampleValues
+            }
+        };
+
+        var bubbleChart = [trace2]
+
+        var layout2 = {
+            title: 'All Sample Sizes',
+            showlegend: false
         }
-      };
-  
-      var data = [bubble];
-      var layout = {
-        title: "Belly Button Bacteria",
-        xaxis: {title: "OTU ID"}
-      };
-      Plotly.newPlot("bubble", data, layout);
-  
-      //Build a Pie Chart
-      d3.json(plotData).then(function(data){
-        var values = data.sample_values.slice(0,10);
-        var labels = data.otu_ids.slice(0,10);
-        var display = data.otu_labels.slice(0,10);
-  
-        var pie_chart = [{
-          values: values,
-          lables: labels,
-          hovertext: display,
-          type: "pie"
-        }];
-        Plotly.newPlot('pie',pie_chart);
-      });
+
+        Plotly.newPlot('bubble', bubbleChart, layout2)
     });
-  };
-  
-  
-      // otu_ids, and labels (10 each).
-  
-  function init() {
-    console.log('hello');
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  
-      // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
-    });
-  }
-  
-  function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-  }
-  
-  // Initialize the dashboard
-  init();
+};
